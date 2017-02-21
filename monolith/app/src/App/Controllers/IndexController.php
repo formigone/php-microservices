88fc0f75d687695f6c92e7cf69cc0b2a29336eef
Rodrigo Silveira <rsilveira@deseretdigital.com>
2017-02-21 06:55:51 -0700

    render news items

--- monolith/app/src/App/Controllers/IndexController.php
+++ monolith/app/src/App/Controllers/IndexController.php
@@ -22,11 +22,21 @@ class IndexController implements ControllerProviderInterface
         $controller = $app['controllers_factory'];
         $controller->get('/', function (Application $app) {
             $data = [
-                'rnd' => [],
+                'headlines' => [
+                    [
+                        'img' => '/img/holidays.jpg',
+                        'title' => 'Thousands get ready for holidays',
+                        'summary' => 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+                        'date' => strtotime('-' . mt_rand(1, 24) . 'hours', time()),
+                    ],
+                    [
+                        'img' => '/img/heavy-traffic.jpg',
+                        'title' => 'Heavy traffic as people drive home',
+                        'summary' => 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+                        'date' => strtotime('-' . mt_rand(1, 24) . 'hours', time()),
+                    ],
+                ],
             ];
-            for ($i = 0; $i < 10; $i++) {
-                $data['rnd'][] = mt_rand(0, 100);
-            }
             return $app['templating']->render(self::MAIN_TEMPLATE, $data);
         });
         return $controller;
